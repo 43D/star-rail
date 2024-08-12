7540325865e19b199366d4caebe3f81f2bdf0f9d
Allan <allan.goioere@hotmail.com>
2024-08-12 00:00:24 -0300

    Update Modal

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -58,15 +58,48 @@ export const NavBar = ({ _setTab, _searchParse, _setSearchParse }: props) => {
                         </li>
 
                     </ul>
-
-                    <form onSubmit={submitSearch} className="d-flex" role="search">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" onChange={onChangeSearchParse} value={_searchParse} />
-                        <button type="submit" className="btn btn-outline-primary">
-                            <i className="bi bi-search"></i>
+                    <div className="d-flex" >
+                        <form onSubmit={submitSearch} className="d-flex" role="search">
+                            <input className="form-control me-1" type="search" placeholder="Search" aria-label="Search" onChange={onChangeSearchParse} value={_searchParse} />
+                            <button type="submit" className="btn btn-outline-primary">
+                                <i className="bi bi-search"></i>
+                            </button>
+                        </form>
+                        <button className="btn ms-2 btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#modalSettings">
+                            <i className="bi bi-gear"></i>
                         </button>
-                    </form>
+                    </div>
                 </div>
             </div>
         </nav>
+        <div className="modal fade" id="modalSettings" tabIndex={-1}>
+            <div className="modal-dialog modal-dialog-centered">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title">
+                            <i className="bi bi-gear me-2"></i>
+                            Configurações
+                        </h5>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div className="modal-body">
+                        <div className="input-group mb-3">
+                            <div className="input-group-text px-1">
+                                <div className="form-check form-switch d-flex align-items-center">
+                                    <input className="form-check-input" type="checkbox" role="switch" id="checkBoxBetaContent" />
+                                </div>
+                            </div>
+                            <label className="input-group-text" htmlFor="checkBoxBetaContent" style={{width: "calc(100% - 50px)"}}>Ativar conteúdos não lançados</label>
+                        </div>
+                        <div className="input-group mb-3">
+                            <label className="input-group-text" htmlFor="selectLanguage">Idioma</label>
+                            <select className="form-select" id="selectLanguage">
+                                <option value="1">Português</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </>);
 }
