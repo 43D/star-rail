c5af8cef9607bfb0ccf0495d762549b99b1f3b50
Allan Fel <allan.goioere@hotmail.com>
2025-02-18 11:41:16 -0300

    parcial update security

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -5,6 +5,7 @@ import { NavBar } from "../components/menu";
 import { FooterIndex } from "../components/footer";
 import { DatabaseRouteSettings } from "./database/DatabaseRouteSettings";
 import { GuidesRouteSettings } from "./guides/GuidesRouteSettings";
+import React from "react";
 
 type props = {
   injectAPI: ApiInject;
@@ -14,15 +15,15 @@ type props = {
 
 export type RoutesProps = {
   injectAPI: ApiInject;
-  nav: JSX.Element;
-  footer: JSX.Element;
+  nav: React.JSX.Element;
+  footer: React.JSX.Element;
   _observable: number;
   injectAdapter: AdapterInject;
 }
 
 
 
-export const defaultTemplate = (mainElement: JSX.Element, nav: JSX.Element, footer: JSX.Element) => {
+export const defaultTemplate = (mainElement: React.JSX.Element, nav: React.JSX.Element, footer: React.JSX.Element) => {
   footer;
 
   return (<>
